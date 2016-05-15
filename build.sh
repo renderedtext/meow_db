0146538ce1124c261415c941c3964e2194546ca5
Igor Šarčević <igor@renderedtext.com>
2016-05-15 17:58:49 +0200

    Move zen.c -> zen-server.c

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 mkdir -p build
 
-gcc -Wall lib/*.c zen.c -I headers -o build/zen `pkg-config --cflags --libs glib-2.0`
+gcc -Wall lib/*.c zen-server.c -I headers -o build/zen-server `pkg-config --cflags --libs glib-2.0`
